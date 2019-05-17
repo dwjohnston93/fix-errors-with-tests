3130d87e59d879523b70b81bfdb68b5d896bb8fe
dwjohnsto93 <dwjohnston93@gmail.com>
2019-05-17 14:10:35 -0700

    Update to lesson12.js and add test for it
    -rename lesson12.js to 13-lesson.js
    -tests output for names other than the inputted name to the function

---
+++ 13-lesson.js
@@ -0,0 +1,28 @@
+//Desired result Print out all names except the name passed into the function 
+
+var people = [
+    { name: "John", age: 55 },
+    { name: "Roger", age: 44 },
+    { name: "Betty", age: 37 }
+    ]
+
+var nameFilter = function(filteredName) {
+    var results = []
+    for (var i = 0; i < people.length; i++) {
+        let name = people[i].name
+        if (name != filteredName) {
+            results.push("My name is " + name)
+        }
+    }
+    console.log("results:", results)
+    return results
+}
+var selectName = function() {
+    var currentName = "Betty";
+    return currentName
+}
+var filteredNames = nameFilter(currentName)
+console.log(filteredNames)
+
+//do not edit
+module.exports = nameFilter; 
