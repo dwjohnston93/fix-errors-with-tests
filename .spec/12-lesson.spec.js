00bcfc0e301e553f03adc70f3aa0acb2e0274428
dwjohnsto93 <dwjohnston93@gmail.com>
2019-05-17 13:59:16 -0700

    Update to lesson11.js and add test for it
    -rename lesson11.js to 12-lesson.js
    -tests output for names other than the inputted name to the function

---
+++ .spec/12-lesson.spec.js
@@ -0,0 +1,13 @@
+const nameFilter = require('../12-lesson.js')
+
+var people = [
+    { name: "John", age: 55 },
+    { name: "Roger", age: 44 },
+    { name: "Betty", age: 37 }
+    ]
+
+test("prints all names of people who aren't the passed in name to the function", () =>{
+    expect(nameFilter("Roger")).toContain("My name is John", "My name is Betty")
+    expect(nameFilter("Roger")).not.toContain('My name is undefined'); 
+    expect(nameFilter("Roger")).not.toContain('My name is Roger'); 
+})
---
+++ 12-lesson.js
@@ -0,0 +1,24 @@
+//Desired result Print out all names except the name passed into 
+//the nameFilter function 
+
+var people = [
+    { name: "John", age: 55 },
+    { name: "Roger", age: 44 },
+    { name: "Betty", age: 37 }
+    ]
+
+var nameFilter = function(filteredName) {
+    var results = []
+    for (var i = 0; i < people.length; i++) {
+        let name = people[i].name
+        if (name != filteredName) {
+            results.push("My name is " + name)
+        }
+    } 
+}
+
+var filteredNames = nameFilter("Betty")
+console.log(filteredNames)
+
+//do not edit
+module.exports = nameFilter; 
