7bac7289caa1321b411012e2df7d50a30c7b0abf
dwjohnsto93 <dwjohnston93@gmail.com>
2019-05-16 23:10:21 -0700

    Update lesson9.js and add test for it
    -rename lesson9.js to 10-lesson.js
    -nameFilter function now returns an output array
    -tests output to return all names other than the one inputted into the
    function

---
+++ 10-lesson.js
@@ -0,0 +1,27 @@
+/*Desired result Print out all names except the name passed into the function 
+                        ######NOTE####### 
+let is just another way to assign a variable like var except 
+it is locally scoped to the function. Var would assign our variable Globally in
+our code.
+*/
+var people = [
+    { name: "John", age: 55 },
+    { name: "Roger", age: 44 },
+    { name: "Betty", age: 37 }
+    ]
+
+var nameFilter = function(name) {
+    let output = [];
+    let filteredName = name
+    for (let i = 0; i < people.length; i++) {
+        let name = people[i].name
+        if (name != filteredName) {
+            output.push("My name is " + name)
+        }
+    } return output
+}
+
+nameFilter("Roger")
+
+//do not edit
+module.exports = nameFilter; 
